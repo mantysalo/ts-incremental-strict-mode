612a9cdf2e076119dfa306e36287338a8794aa12
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-10-27 16:31:55 +0200

    remove config path

--- src/__tests__/getTSConfig.test.ts
+++ src/__tests__/getTSConfig.test.ts
@@ -1,9 +1,8 @@
 import { getTSConfig } from '../cli';
-
-jest.mock('fs');
+import { resolve } from 'path';
 
 describe('getTSConfig', () => {
     it('returns a config path', () => {
-        expect(getTSConfig('./tsconfig.json')).resolves.toMatch('./tsconfig.json');
+        expect(getTSConfig()).resolves.toMatch(resolve('./tsconfig.json'));
     });
 });
