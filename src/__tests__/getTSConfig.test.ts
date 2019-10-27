706ce65603d3e6c313544b46e2f07fcf1c57ac34
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-10-27 16:32:23 +0200

    add test for invalid config path

--- src/__tests__/getTSConfig.test.ts
+++ src/__tests__/getTSConfig.test.ts
@@ -5,4 +5,15 @@ describe('getTSConfig', () => {
     it('returns a config path', () => {
         expect(getTSConfig()).resolves.toMatch(resolve('./tsconfig.json'));
     });
+
+    it('throws an error if config path is invalid', async () => {
+        try {
+            await getTSConfig('./nosuchpath/tsconfig.json');
+        } catch (error) {
+            expect(error.code).toEqual('ENOENT');
+            expect(error.message).toEqual(
+                "ENOENT: no such file or directory, access './nosuchpath/tsconfig.json'"
+            );
+        }
+    });
 });
