ef72b174b8cd4fe2b4a8fb0e1551bc301ca0c38b
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-05 16:44:33 +0200

    add override for inherited include property

--- src/baseConfig.ts
+++ src/baseConfig.ts
@@ -3,6 +3,7 @@ import { FilePath } from './cli';
 export const generateTempConfig = (existingConfigPath: FilePath, files: FilePath[]): string => {
     return JSON.stringify({
         extends: existingConfigPath,
-        files
+        files,
+        include: []
     });
 };
