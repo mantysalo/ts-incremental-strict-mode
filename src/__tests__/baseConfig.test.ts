1f71e532c1e4dddf517d628e6673461e18c87790
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-05 16:59:55 +0200

    update assertion to reflect changes in ef72b17

--- src/__tests__/baseConfig.test.ts
+++ src/__tests__/baseConfig.test.ts
@@ -3,7 +3,7 @@ import { generateTempConfig } from '../baseConfig';
 describe('generateTempConfig', () => {
     it('creates a valid tsconfig', () => {
         expect(generateTempConfig('./tsconfig.json', ['file1.ts'])).toEqual(
-            '{"extends":"./tsconfig.json","files":["file1.ts"]}'
+            '{"extends":"./tsconfig.json","files":["file1.ts"],"include":[]}'
         );
     });
 });
