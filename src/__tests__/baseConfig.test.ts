15f524945afba92983aec8192e36083c3629af6e
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-07 21:33:43 +0200

    mock writefile
    avoid creating tsconfigs when running tests

--- src/__tests__/baseConfig.test.ts
+++ src/__tests__/baseConfig.test.ts
@@ -1,7 +1,9 @@
 import { generateTempConfig } from '../baseConfig';
+import fs from 'fs';
 
 describe('generateTempConfig', () => {
     it('creates a valid tsconfig', () => {
+        jest.spyOn(fs, 'writeFile').mockImplementation(() => {});
         expect(generateTempConfig('./tsconfig.json', ['file1.ts'])).toEqual(
             '{"extends":"./tsconfig.json","files":["file1.ts"],"include":[]}'
         );
--- src/__tests__/createTempTSConfig.test.ts
+++ src/__tests__/createTempTSConfig.test.ts
@@ -1,7 +1,8 @@
 import { createTempTSConfig } from '../cli';
-
+import fs from 'fs';
 describe('createTempTSConfig', () => {
     it('creates a temporary tsconfig', async () => {
+        jest.spyOn(fs, 'writeFile').mockImplementation(() => {});
         expect(await createTempTSConfig(['file1.ts', 'file2.ts'])).toContain('tsconfig.temp.json');
     });
 });
